b1f1064eafe701008f7299252d9fd84be08ebf5e
Jeff Bush <jeffbush001@gmail.com>
2013-04-09 21:52:58 -0700

    Slap on license boilerplate

--- vga_timing_generator.v
+++ vga_timing_generator.v
@@ -1,5 +1,5 @@
 // 
-// Copyright 2012 Jeff Bush
+// Copyright 2013 Jeff Bush
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,6 +13,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 // 
+
+
 
 module vga_timing_generator(
 	input clk, 
