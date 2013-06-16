be9e515ba5b58d29696a20314d85314addb1ee81
Jeff Bush <jeffbush001@gmail.com>
2013-06-16 08:59:42 -0700

    Use generate to simplify instantiations of pixel ALUs.

--- pixel_processor.v
+++ pixel_processor.v
@@ -17,7 +17,8 @@
 
 module pixel_processor
 	#(parameter NUM_PIXELS = 8,
-	parameter OUTPUT_WIDTH = NUM_PIXELS * 12)
+	parameter PIXEL_WIDTH = 12,
+	parameter OUTPUT_WIDTH = NUM_PIXELS * PIXEL_WIDTH)
 
 	(input clk,
 	input new_frame,
@@ -29,80 +30,32 @@ module pixel_processor
 
 	reg[3:0] pc;
 	wire[INSTRUCTION_WIDTH - 1:0] instruction;
-	reg[31:0] x_coord0;
-	reg[31:0] x_coord1;
-	reg[31:0] x_coord2;
-	reg[31:0] x_coord3;
-	reg[31:0] x_coord4;
-	reg[31:0] x_coord5;
-	reg[31:0] x_coord6;
-	reg[31:0] x_coord7;
 	reg[31:0] y_coord;
 	reg[31:0] f_number;
 
-	pixel_alu pixel_alu0(
-		.clk(clk),
-		.instruction(instruction),
-		.x_coord(x_coord0),
-		.y_coord(y_coord),
-		.f_number(f_number),
-		.output_value(result[11:0]));
+	genvar lane;
+	generate
+		for (lane = 0; lane < NUM_PIXELS; lane = lane + 1)
+		begin : pixel_compute
+			reg[31:0] x_coord = 0;
+			
+			always @(posedge clk)
+			begin
+				if (new_frame || (end_of_line && start_next_batch))
+					x_coord <= lane;
+				else if (start_next_batch)
+					x_coord <= x_coord + NUM_PIXELS;
+			end
 
-	pixel_alu pixel_alu1(
-		.clk(clk),
-		.instruction(instruction),
-		.x_coord(x_coord1),
-		.y_coord(y_coord),
-		.f_number(f_number),
-		.output_value(result[23:12]));
-
-	pixel_alu pixel_alu2(
-		.clk(clk),
-		.instruction(instruction),
-		.x_coord(x_coord2),
-		.y_coord(y_coord),
-		.f_number(f_number),
-		.output_value(result[35:24]));
-
-	pixel_alu pixel_alu3(
-		.clk(clk),
-		.instruction(instruction),
-		.x_coord(x_coord3),
-		.y_coord(y_coord),
-		.f_number(f_number),
-		.output_value(result[47:36]));
-
-	pixel_alu pixel_alu4(
-		.clk(clk),
-		.instruction(instruction),
-		.x_coord(x_coord4),
-		.y_coord(y_coord),
-		.f_number(f_number),
-		.output_value(result[59:48]));
-
-	pixel_alu pixel_alu5(
-		.clk(clk),
-		.instruction(instruction),
-		.x_coord(x_coord5),
-		.y_coord(y_coord),
-		.f_number(f_number),
-		.output_value(result[71:60]));
-
-	pixel_alu pixel_alu6(
-		.clk(clk),
-		.instruction(instruction),
-		.x_coord(x_coord6),
-		.y_coord(y_coord),
-		.f_number(f_number),
-		.output_value(result[83:72]));
-
-	pixel_alu pixel_alu7(
-		.clk(clk),
-		.instruction(instruction),
-		.x_coord(x_coord7),
-		.y_coord(y_coord),
-		.f_number(f_number),
-		.output_value(result[95:84]));
+			pixel_alu pixel_alu0(
+				.clk(clk),
+				.instruction(instruction),
+				.x_coord(x_coord),
+				.y_coord(y_coord),
+				.f_number(f_number),
+				.output_value(result[lane * PIXEL_WIDTH+:PIXEL_WIDTH]));
+		end
+	endgenerate
 
 	sram_1r1w #(INSTRUCTION_WIDTH, 16) instruction_mem(
 		.clk(clk),
@@ -116,19 +69,11 @@ module pixel_processor
 	begin
 		pc = 0;
 		y_coord = 0;
-		x_coord0 = 0;
-		x_coord1 = 0;
-		x_coord2 = 0;
-		x_coord3 = 0;
-		x_coord4 = 0;
-		x_coord5 = 0;
-		x_coord6 = 0;
-		x_coord7 = 0;
 		f_number = 0;
 	end
 
 	assign result_ready = pc == 4'b1111;
-	wire end_of_line = x_coord0 == 640 - NUM_PIXELS;
+	wire end_of_line = pixel_compute[0].x_coord == 640 - NUM_PIXELS;
 
 	always @(posedge clk)
 	begin
@@ -144,28 +89,5 @@ module pixel_processor
 		end
 		else if (end_of_line && start_next_batch)
 			y_coord <= y_coord + 1;
-	
-		if (new_frame || (end_of_line && start_next_batch))
-		begin
-			x_coord0 <= 0;
-			x_coord1 <= 1;
-			x_coord2 <= 2;
-			x_coord3 <= 3;
-			x_coord4 <= 4;
-			x_coord5 <= 5;
-			x_coord6 <= 6;
-			x_coord7 <= 7;
-		end
-		else if (start_next_batch)
-		begin
-			x_coord0 <= x_coord0 + NUM_PIXELS;
-			x_coord1 <= x_coord1 + NUM_PIXELS;
-			x_coord2 <= x_coord2 + NUM_PIXELS;
-			x_coord3 <= x_coord3 + NUM_PIXELS;
-			x_coord4 <= x_coord4 + NUM_PIXELS;
-			x_coord5 <= x_coord5 + NUM_PIXELS;
-			x_coord6 <= x_coord6 + NUM_PIXELS;
-			x_coord7 <= x_coord7 + NUM_PIXELS;
-		end
 	end
 endmodule
